5894cde5844b201d5e27ae9067a44225453a4f7e
Daniel Farina <splitfirex@gmail.com>
2015-03-09 22:00:37 +0100

    Cambio d elibreria Jquery por Jquery Mobile

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,4 @@
  $(function() {
-    	$( "#effect" ).hide( "blind", {}, 1000, callback );
-    
-    	function callback() {
-    	      setTimeout(function() {
-    	        $( "#effect" ).removeAttr( "style" ).hide().fadeIn();
-    	      }, 1000 );
-    	    };
+	 $( "#mypanel" ).panel( "open" );
     	
     });
