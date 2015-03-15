cfae32acfd1dbcea9c7f5021ff5591c4765ae41c
Daniel Farina <splitfirex@gmail.com>
2015-03-15 13:50:31 +0100

    Incluye mas funcionalidades - Permite cargar Epubs desde una carpeta - Muestra los nombres y las portadas de los epubs

--- js/myjs.js
+++ js/myjs.js
@@ -4,6 +4,33 @@ $(function () {
     return;
   }
 
+ function encode (input) {
+    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var output = "";
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+    var i = 0;
+
+    while (i < input.length) {
+        chr1 = input[i++];
+        chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index 
+        chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
+
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+
+        if (isNaN(chr2)) {
+            enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+            enc4 = 64;
+        }
+        output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+                  keyStr.charAt(enc3) + keyStr.charAt(enc4);
+    }
+    return output;
+}
+	var imagenes = 0;
 
   var $result = $("#result");
   $("#file").on("change", function(evt) {
@@ -49,7 +76,17 @@ $(function () {
             		  $fileContent.append($("<li>", {
                           text : $title[0].innerHTML
                         }));
+						
             		//alert("titulo " + $title);
+            	}
+            	if(zipEntry.name.indexOf("cover.jpg") > -1){
+            		imagenes = imagenes +1;
+
+            			// var blob = new Blob(zipEntry.asBinary(), {type: 'application/octet-binary'});
+            			// var url = URL.createObjectURL(blob);
+            		  $fileContent.append($("<img>",{
+            		  	src : 'data:image/png;base64,'+encode(zipEntry.asUint8Array())
+            		  }));
             	}
             	
 //              $fileContent.append($("<li>", {
@@ -69,6 +106,7 @@ $(function () {
             });
           }
           $result.append($fileContent);
+          alert(imagenes);
         }
       })(f);
 
